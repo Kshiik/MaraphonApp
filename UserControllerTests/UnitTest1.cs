4c2180d340ca4ce84c42152862f77dd248e876c2
rotoyouoi <rotoyouoi@yandex.ru>
2023-11-21 15:10:15 +0500

    Добавьте файлы проекта.

---
+++ UserControllerTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MaraphonApp;
+using MaraphonApp.Controllers;
+
+namespace UserControllerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string email = "abcd@gmail.com";
+            string firstName = "Ivan";
+            string lastName = "Ivanov";
+            string role = "Бегун";
+            string gender = "Мужской";
+            string password = "Password123*";
+
+            bool result = UserController.SaveRunnerData(email, firstName, lastName, role, gender, password);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
